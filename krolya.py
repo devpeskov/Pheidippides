2ae3abf9148d68cb8f0050975c283e4fb985327c
inauris <inauris@protonmail.com>
2022-03-11 18:55:00 +0600

    Add smart stickers

--- krolya.py
+++ krolya.py
@@ -2,40 +2,52 @@ import telebot
 import config
 import requests
 from decimal import Decimal
-import asyncio
-import aioschedule as schedule
-import time
+# import asyncio
+# import aioschedule as schedule
+# import time
 import random
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
-def sendCurrency(chat='-1001215138602'):
+# chats-id:
+# fpc(default) = '-1001798667684'
+# fpooop = '-1001691787586'
+def sendCurrency(chat='-1001798667684'):
+    dumpJson = requests.get(
+        'https://api.coingecko.com/api/v3/coins',
+        ).json()
+    stickerSwitch = dumpJson[0]['market_data']['price_change_percentage_24h']
     msg = 'Ныне курс такой:\n\n'
-    msg += getCryptoSchedule('bitcoin', 'Биток биток биточек')
-    msg += getCryptoSchedule('ethereum', 'Бутырка')
-    msg += getCryptoSchedule('monero', 'Нидзямонета')
+    msg += formMessage('bitcoin', 'Батькоин', dumpJson)
+    msg += formMessage('ethereum', 'Эфирка', dumpJson)
+    msg += formMessage('monero', 'Нидзямонета', dumpJson)
 
-    stickerName = str(random.randrange(1, 19))
-    sti = open(f'static/{stickerName}.webp', 'rb')
-    bot.send_sticker(chat, sti)
+    bot.send_sticker(chat, chooseSticker(stickerSwitch))
     bot.send_message(chat, msg)
-    # fpc = '-1001215138602'
-    # fpooop = '-1001691787586'
 
 
-def getCryptoSchedule(cryptoName, message, baseCurrency='usd'):
+def formMessage(cryptoName, message, dumpJson, baseCurrency='usd'):
     '''Parses json response and outputs value to polybar'''
-    json = requests.get(
-            f'https://api.coingecko.com/api/v3/coins/{cryptoName}',
-            ).json()["market_data"]
-    local_price = round(Decimal(json["current_price"][f'{baseCurrency}']), 2)
-    change_24h = round(float(json["price_change_percentage_24h"]), 2)
+    coinJson = []
+    for json in dumpJson:
+        if json['id'] == cryptoName:
+            coinJson = json['market_data']
 
-    str = (
-        f'{message}: {local_price} ({change_24h:+})\n'
-    )
-    return str
+    local_price = round(Decimal(coinJson["current_price"][f'{baseCurrency}']), 2)
+    change_24h = round(float(coinJson["price_change_percentage_24h"]), 2)
+    return f'{message}: {local_price} ({change_24h:+})\n'
+
+
+def chooseSticker(change_24h):
+    if change_24h >= 0:
+        stickerName = str(random.randrange(1, 7))
+        sti = open(f'static/good/{stickerName}.webp', 'rb')
+        return sti
+    else:
+        stickerName = str(random.randrange(1, 13))
+        sti = open(f'static/bad/{stickerName}.webp', 'rb')
+        return sti
 
 
 def getCrypto(cryptoName, baseCurrency='usd'):
@@ -47,7 +59,7 @@ def getCrypto(cryptoName, baseCurrency='usd'):
     return local_price
 
 
-schedule.every(10).seconds.do(sendCurrency)
+# schedule.every(10).seconds.do(sendCurrency)
 # schedule.every(10).minutes.do(sendCurrency)
 # schedule.every().hour.do(sendCurrency)
 # schedule.every().day.at("10:30").do(sendCurrency)
@@ -56,10 +68,10 @@ schedule.every(10).seconds.do(sendCurrency)
 # schedule.every().wednesday.at("13:15").do(sendCurrency)
 # schedule.every().minute.at(":17").do(sendCurrency)
 
-loop = asyncio.get_event_loop()
-while True:
-    loop.run_until_complete(schedule.run_pending())
-    time.sleep(0.1)
+# loop = asyncio.get_event_loop()
+# while True:
+#     loop.run_until_complete(schedule.run_pending())
+#     time.sleep(0.1)
 
 
 @bot.message_handler(commands=['start'])
@@ -85,8 +97,8 @@ def sendCrypto(message):
         bot.send_sticker(message.chat.id, sti)
         bot.send_message(message.chat.id, msg)
     elif message.text == 'Кроля, дай многа монеток':
-        msg = f'Ох... Ладно\n\nБиток биток биточек: {getCrypto("bitcoin")}\n' \
-                  f'Бутырка: {getCrypto("ethereum")}\n' \
+        msg = f'Ох... Ладно\n\nБатькоин: {getCrypto("bitcoin")}\n' \
+                  f'Эфирка: {getCrypto("ethereum")}\n' \
                   f'Нидзямонета: {getCrypto("monero")}\n'
         sti = open('static/4.webp', 'rb')
         bot.send_sticker(message.chat.id, sti)
