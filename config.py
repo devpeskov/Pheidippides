15c590ea7cd75dac87cb198caa56d5d462682656
inauris <inauris@protonmail.com>
2022-05-29 14:17:21 +0600

    Changed pip to poetry + token to .env

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import getenv
+from os.path import dirname, join
+
+from dotenv import load_dotenv  # type: ignore
+
+load_dotenv(join(dirname(__file__), './.env'))
+
+TOKEN = getenv('TOKEN')
